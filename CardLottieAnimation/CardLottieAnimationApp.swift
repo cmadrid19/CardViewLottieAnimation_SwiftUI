d0dc795041684d88369364085c3e33f18c5e3e09
Max <maxim.macari.dam@gmail.com>
2020-10-06 20:32:35 +0200

    Initial Commit

---
+++ CardLottieAnimation/CardLottieAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardLottieAnimationApp.swift
+//  CardLottieAnimation
+//
+//  Created by Maxim Macari on 06/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct CardLottieAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
